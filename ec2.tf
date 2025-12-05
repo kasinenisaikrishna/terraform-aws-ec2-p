ef3f88c6d750c6dfc8db0060de82504eb02a4b8a
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-12-05 17:43:21 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform"{
-    ami = "ami-09c813fb71547fc4f"
+    ami = var.ami_id
     instance_type = "t3.micro"
     vpc_security_group_ids = ["sg-001c2affbe1e077cf"]
 }
